2a27cb3b8776517c8095ff68d55294a15acf5c1c
James Newton <github@lexogram.com>
2015-06-15 03:49:24 +0200

    simplified css

---
+++ nim/Steps/step_101/js/nim.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var rows = document.querySelectorAll(".matches");
+var total = document.querySelectorAll(".matches img").length;
+//console.log(matches);
+
+for (var ii=0; ii<rows.length; ii++) {
+  var row = rows[ii];
+
+  row.onclick = hideMatch;
+}
+
+function hideMatch(event) {
+  var match = event.target;
+  match.classList.add("removed");
+
+  var selector = ".matches img.removed";
+  var removed = document.querySelectorAll(selector).length;
+  if (removed === total) {
+    window.alert("You lose.");
+  }
+}
+
+function reset() {
+  var matches = document.querySelectorAll(".matches img.removed");
+
+  for (var ii=0; ii<matches.length; ii++) {
+    var match = matches[ii];
+    match.classList.remove("removed");
+  }
+}
