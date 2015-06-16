2bdffadc350a01f7a17846109f743f1934691420
James Newton <bitbucket@lexogram.com>
2015-06-16 08:43:13 -0400

    Add step_012
     - Whole script wrapped in (function ready() {...})() to remove variables from global scope
     - initialize() created to assemble all start-up code
     - var declarations placed at the top
     - methods reordered
     - toggleNextTurn() added
     - calls to toggleNextTurn() added to initialize(), hideMatch() and reset()
    
     (Players can still take matches from more than one line)

---
+++ nim/Steps/step_12/js/nim.js
@@ -0,0 +1,89 @@
+"use strict";
+
+;(function ready() {
+
+  var rows = document.querySelectorAll(".matches");
+  var total = document.querySelectorAll(".matches img").length;
+  var turns = document.querySelectorAll(".turns button");
+  var start = document.querySelectorAll(".start button")[0];
+  var players = document.querySelectorAll(".turns div");
+
+
+  function initialize() {
+    var button, turn
+
+    for (var ii=0; ii<rows.length; ii++) {
+      var row = rows[ii];
+
+      row.onclick = hideMatch;
+    }
+
+    for (var ii=0; ii<turns.length; ii++) {
+      turn = turns[ii];
+      turn.onclick = nextTurn;
+    }
+
+    start.disabled = true;
+    toggleNextTurn(false);
+  }
+
+  function hideMatch(event) {
+    var match = event.target;
+    match.classList.add("removed");
+
+    var selector = ".matches img.removed";
+    var removed = document.querySelectorAll(selector).length;
+    if (removed === total) {
+      window.alert("You lose.");
+    }
+
+    start.disabled = false;
+    toggleNextTurn(true)
+  }
+
+  function toggleNextTurn(enabled) {
+    var turn;
+
+    for (var ii=0; ii<turns.length; ii++) {
+      turn = turns[ii];
+      turn.disabled = !enabled
+    }
+
+  }
+
+  function nextTurn(event) {
+    var active = document.querySelector(".turns div.active");
+    var next = document.querySelector(".turns div:not(.active)");
+
+    active.classList.remove("active");
+    next.classList.add("active");
+
+    toggleNextTurn(false)
+  }
+ 
+  function reset(event) {
+    event = event || window.event
+
+    var matches = document.querySelectorAll(".matches img.removed");
+    var match, player
+
+    for (var ii=0; ii<matches.length; ii++) {
+      match = matches[ii];
+      match.classList.remove("removed");
+    }
+
+    for (var ii=0; ii<players.length; ii++) {
+      player = players[ii];
+      if (ii === 0) {
+        player.classList.add("active");
+      } else {
+        player.classList.remove("active");
+      }
+    }
+
+    start.disabled = true;
+    toggleNextTurn(false)
+  }
+
+  initialize();
+})()
